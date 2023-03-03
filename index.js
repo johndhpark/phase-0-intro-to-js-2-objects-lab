0202293fb40f6a9d0a6cba8512b2173de360b434
John DH. Park <john.dh.park@me.com>
2023-03-02 17:10:56 -0800

    Added the required functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = { name: "Same" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+	const updated = { ...employee };
+	updated[key] = value;
+
+	return updated;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+	employee[key] = value;
+
+	return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+	const deleted = { ...employee };
+	delete deleted[key];
+
+	return deleted;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+	delete employee[key];
+
+	return employee;
+}
